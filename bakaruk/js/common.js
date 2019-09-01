3b7c850012b12dbb07b4fa2798414790a01992d1
Hemlet <(hemlet88@gmail.com>
2019-09-01 16:01:17 +0300

    s
    s

--- bakaruk/js/common.js
+++ bakaruk/js/common.js
@@ -69,18 +69,3 @@ $grid.masonry( 'on', 'layoutComplete', function() {
 // trigger initial layout
 $grid.masonry();
 
-var touchHover = function() {
-    $('[data-hover]').click(function(e){
-        e.preventDefault();
-        var $this = $(this);
-        var onHover = $this.attr('data-hover');
-        var linkHref = $this.attr('href');
-        if (linkHref && $this.hasClass(onHover)) {
-            location.href = linkHref;
-            return false;
-        }
-        $this.toggleClass(onHover);
-    });
-};
-
-$(document).ready(function() { touchHover(); });
