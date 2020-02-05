daa768db2fc2000cc292643e595a3e04c1805885
Hemlet <(hemlet88@gmail.com>
2020-02-05 21:21:34 +0300

    a
    a

--- catenergy/js/common.js
+++ catenergy/js/common.js
@@ -1,8 +1,12 @@
 var link = document.querySelector(".mobile-menu");
 var menu = document.querySelector(".top-menu");
-var close = document.querySelector(".mobile-menu-show");
+var closeBtn = document.getElementById("botton-menu");
+
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	menu.classList.add("menu-show");
+	menu.classList.toggle("menu-show");
+	closeBtn.classList.toggle("mobile-menu-open");
+	closeBtn.classList.toggle("mobile-menu-show");
 });
+
