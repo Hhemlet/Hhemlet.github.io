52bd903373e0859aa9def83b6f29bb01e93a97ab
Hemlet <(hemlet88@gmail.com>
2020-03-22 21:01:22 +0300

    as
    as

--- qwert/common.js
+++ qwert/common.js
@@ -1,13 +1,62 @@
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
-let image = document.querySelectorAll("img");
-let start = document.querySelectorAll(".slider > img:first-child");
-let end = document.querySelectorAll(".slider > img:last-child");
+let image = document.getElementsByTagName("img");
+let currentElementSlider = 0;
 
-if (start) {
-	prev.setAttribute("disabled", "disabled");
-};
+prev.disabled = true;
 
-if (end) {
-	next.setAttribute("disabled", "disabled");
-};
+next.addEventListener("click", function() {
+			currentElementSlider++
+		if (currentElementSlider > 0) {
+			prev.disabled = false }
+			if (currentElementSlider == (image.length - 1)) {
+				next.disabled = true 
+			} else { next.disabled = false 
+				};
+	
+		for (let i = 0; i < image.length; i++) {
+			image[i].classList.add("hidden");
+			image[currentElementSlider].classList.remove("hidden");
+			console.log(currentElementSlider);
+
+	};
+
+});
+	
+
+prev.addEventListener("click", function() {
+			currentElementSlider--
+	if (currentElementSlider < (image.length - 1)) {
+		next.disabled = false }
+		if (currentElementSlider == 0 ) {
+			prev.disabled = true 
+			} else {prev.disabled = false
+				};
+
+		for (let i = image.length - 1; i >= 0; i--) {
+			image[i].classList.add("hidden");
+			image[currentElementSlider].classList.remove("hidden");
+			console.log(currentElementSlider);
+	};
+
+	
+});
+
+
+
+
+let minus = document.querySelector(".minus");
+let plus = document.querySelector(".plus");
+let input = document.querySelector("input");
+let currentElement = 0;
+input.value = currentElement;
+
+plus.addEventListener("click", function(){
+	if (currentElement <= 9)
+		input.value = ++currentElement;
+})
+
+minus.addEventListener("click", function(){
+	if (currentElement >= 1)
+		input.value = --currentElement;
+})
