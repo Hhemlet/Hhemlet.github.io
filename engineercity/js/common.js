ed0a409f35972bbc9d9d83c2b315c77ea9464f8f
Alexey <(hemlet88@gmail.com>
2020-09-20 20:52:51 +0300

    f
    f

--- engineercity/js/common.js
+++ engineercity/js/common.js
@@ -3,6 +3,32 @@
 $(document).ready(function(){
   $(".single-item").slick({
   	dots: true,
+  	AdaptiveHeight: true
   });
 });
 
+
+$(document).ready(function(){
+  $(".catalog-slider").slick({
+  	infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 1,
+
+  });
+});
+
+let minus = document.querySelector(".minus");
+let plus = document.querySelector(".plus");
+let input = document.querySelector("input");
+let currentElement = 1;
+input.value = currentElement;
+
+plus.addEventListener("click", function(){
+	if (currentElement <= 100)
+		input.value = ++currentElement;
+})
+
+minus.addEventListener("click", function(){
+	if (currentElement >= 2)
+		input.value = --currentElement;
+})
