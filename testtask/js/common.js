863a5cc662d2c2b0d5b11e63131f3af8504d6a7f
Alexey <(hemlet88@gmail.com>
2020-09-23 14:31:45 +0300

    q
    q

--- testtask/js/common.js
+++ testtask/js/common.js
@@ -1,7 +1,10 @@
 const closeBtn = document.querySelector(".hamburger");
-const menu = document.querySelector(".main-nav")
+const menu = document.querySelector(".main-nav");
+const body = document.querySelector("body");
 
 
 closeBtn.addEventListener("click", function () {
 	menu.classList.toggle("menu-hidden");
+	body.classList.toggle("fixed")
+
 });
