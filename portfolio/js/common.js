33568d8f4943a3c5d7bf8c43d35119a34434d867
Alexey <(hemlet88@gmail.com>
2020-10-11 14:46:38 +0300

    s
    s

---
+++ portfolio/js/common.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling: true,
+    scrollHorizontally: false,
+    navigation: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fivePage'],
+	menu: '#myMenu'
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
+
+const closeBtn = document.querySelector(".hamburger");
+const menu = document.querySelector(".main-page-nav");
+
+closeBtn.addEventListener("click", function () {
+   
+    menu.classList.toggle("menu-hidden");
+
+});
+
+
+};
